aa7456da9b1b49d8736f712b25eba7b2eb7ad552
Igor Barbashov <bis200705@gmail.com>
2026-04-02 00:03:35 +0300

    Controllers & Minimal APIs

---
+++ WebApplication/WebApplication/Controllers/ProductsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controlles
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Products from controller");
+        }
+    }
+}
