c66dc375c1a4952c0998378c50884ef49f058f9d
Igor Barbashov <bis200705@gmail.com>
2026-04-02 01:36:06 +0300

    classical Mvc Controllers example

---
+++ WebApplication/WebApplication/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CustomerController : ControllerBase
+{
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var products = new[]
+        {
+            new { id = 1, name = "Cocos" },
+            new { id = 2, name = "Ananas" }
+        };
+
+        return Ok();
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var customer = new { id, name = "Name" };
+        return Ok(customer);
+    }
+
+    [HttpGet("{id}/reviews/{reviewId}")]
+    public IActionResult GetById(int id, int reviewId)
+    {
+        var customer = new { id, name = "Name", reviewId };
+        return Ok(customer);
+    }
+}
